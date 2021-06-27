1890620d0ee46cc033ddeb0b283f995d00f74349
xK3LY <kelybiznes@gmail.com>
2021-06-27 22:34:45 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,4 @@
-game 'rdr3'
+game 'gta5'
 
 fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
